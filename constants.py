334b8efeec6676e468905e0e1ce4a5bf946b5e1c
bogap <58374268+bogap@users.noreply.github.com>
2022-01-20 14:05:46 +0300

    8th commit

---
+++ constants.py
@@ -0,0 +1,8 @@
+FPS = 50
+HELLO_BYE = 450
+WINDOW_SIZE = WIDTH, HEIGHT = 30 * 50, 6 * 50  # window size
+SCREEN_TITLE = 'KOTOSHIZA'
+# tile_size
+tile_width = tile_height = 50
+ENEMY_EVENT_TYPE=60
+INF=1000
